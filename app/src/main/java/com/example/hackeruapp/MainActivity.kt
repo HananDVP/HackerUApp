953e4fd3f9141665a8cca3117aebf3fe592aa18d
AvitanH <hananavt@gmail.com>
2022-09-14 11:07:13 +0300

    Lesson 1

---
+++ app/src/main/java/com/example/hackeruapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hackeruapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun getName(): String{
+        var name:String = "Hanan"
+        name = "Hanan Avitan"
+        return name
+    }
+
+
+    fun onButtonClick(view: View) {
+        val textView: TextView = findViewById<TextView>(R.id.hello_text)
+        textView.text = "Hello ${ getName ()} How are you?"
+    }
+}
